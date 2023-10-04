2433d2a4caadd5ac39ded6ffafa6ac9422666be5
Danil Zherebtsov <danil.com@me.com>
2023-10-04 09:17:56 +0400

    ammend readme and thumbnail

--- README.rst
+++ README.rst
@@ -1,5 +1,14 @@
 
 .. image:: thumbnail.png
 
-The repository contains the code supporting a youtube video: https://youtu.be/IxEpU_R3LFw
+The repository contains the code supporting a youtube video `<https://youtu.be/IxEpU_R3LFw>`_.
+
+Prequisits
+----------
+The code is dependent on packages listed in requirements.txt. 
+
+Contents
+--------
+The repository contains code to connect to ChatGPT & Whisper APIs, and a simple flask app to demonstrate the APIs.
+Resulting program provides a web interface which allows user to enter a (verbal) message (question to ChatGPT) which will get transcribed by Whisper via API and then will be redirected to ChatGPT as a question. The response will be returned to the web-app frontend. 
 
--- main.py
+++ main.py
@@ -9,13 +9,29 @@ messages=[
             ]
 
 def speech_to_text(audio):
-    '''Transcribe audio file to text by whisper.'''
+    '''Transcribe an audio file to text using OpenAI's whisper model.
+
+    Args:
+        audio (str): Path to the audio file to transcribe.
+
+    Returns:
+        str: The transcribed text.
+
+    '''
     audio_file = open(audio, "rb")
     transcript = openai.Audio.transcribe('whisper-1', audio_file)
     return transcript['text']
 
 def query_ChatGPT(messages):
-    '''Send messages to OpenAI's Chat GPT-3 model and get response.'''
+    '''Send messages to OpenAI's Chat GPT-3 model and get response.
+
+    Args:
+        messages (list): A list of messages to send to the model.
+
+    Returns:
+        str: The response message from the model.
+
+    '''
     response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
         messages=messages
@@ -24,7 +40,17 @@ def query_ChatGPT(messages):
     return response_message
 
 def create_chat_transcript(messages):
-    '''Create chat transcript from all messages.'''
+    '''Create a chat transcript from all non-system messages.
+
+    Args:
+        messages (list): A list of messages, where each message is a dictionary
+            with keys 'role' and 'content'.
+
+    Returns:
+        str: The chat transcript, where each message is formatted as
+            "<role>: <content>\n\n".
+
+    '''
     chat_transcript = ''
     for message in messages:
         if message['role'] != 'system':
